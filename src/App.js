aafeafeaaf6bfc1fd1c10f298ed254477e72199e
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-03-14 11:34:00 -0300

    foundation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Cadastro from "./pages/Cadastro";
+import Habitos from "./pages/Habitos";
+import Historico from "./pages/Historico";
+import Hoje from "./pages/Hoje";
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Tudo>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/cadastro" element={<Cadastro />}/>
+          <Route path="/habitos" element={<Habitos />}/>
+          <Route path="/hoje" element={<Hoje />}/>
+          <Route path="/historico" element={<Historico />}/>
+        </Routes>
+      </Tudo>
+    </BrowserRouter>
   );
 }
+
+const Tudo = styled.div`
+height: 667px;
+width: 375px;
+background-color: gray;
+`
